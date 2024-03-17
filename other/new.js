4706d0ff86c461906a1436312e0b56b6bd7115b4
BirthX <87570691+BirthScripts@users.noreply.github.com>
2024-03-17 21:22:50 +0800

    Update new.js

--- other/new.js
+++ other/new.js
@@ -1,8 +1,16 @@
-const fullscreenButton = document.getElementById('fullscreen-button');
+    const fullscreenButton = document.getElementById('fullscreen-button');
     const videoContainer = document.getElementById('video-container');
     const video = document.getElementById('myVideo');
+    const videoUrls = [
+        'https://birthscripts.github.io/videos/alien.mp4',
+        'https://birthscripts.github.io/videos/alien2.mp4'
+    ];
 
     fullscreenButton.addEventListener('click', () => {
+        const randomIndex = Math.floor(Math.random() * videoUrls.length);
+        const randomVideoUrl = videoUrls[randomIndex];
+        video.src = randomVideoUrl;
+        
         if (video.requestFullscreen) {
             video.requestFullscreen();
         } else if (video.mozRequestFullScreen) {
@@ -12,6 +20,7 @@ const fullscreenButton = document.getElementById('fullscreen-button');
         } else if (video.msRequestFullscreen) {
             video.msRequestFullscreen();
         }
+        
         video.play();
         videoContainer.style.display = 'block';
         fullscreenButton.style.display = 'none';
