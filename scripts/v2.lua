cb9464f8bbf97202301604112907d7c19052f8d3
BirthX <87570691+BirthScripts@users.noreply.github.com>
2024-03-25 09:59:48 +0800

    Update v2.lua

--- scripts/v2.lua
+++ scripts/v2.lua
@@ -75,7 +75,7 @@ local ScreenGui = Instance.new("ScreenGui")
 ScreenGui.Name = "ScreenGui"
 ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 ScreenGui.ResetOnSpawn = false
- 
+
 local Toggle = Instance.new("TextButton")
 Toggle.Name = "Toggle"
 Toggle.Parent = ScreenGui
@@ -83,14 +83,19 @@ Toggle.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 Toggle.Position = UDim2.new(0, 0, 0.454706937, 0)
 Toggle.Size = UDim2.new(0, 90, 0, 38)
 Toggle.Font = Enum.Font.SourceSans
-Toggle.Text = "Close/Open"
+Toggle.Text = "Close"
 Toggle.TextColor3 = Color3.fromRGB(248, 248, 248)
 Toggle.TextSize = 28.000
 Toggle.Draggable = true
-Toggle.MouseButton1Click:connect(function()
+Toggle.MouseButton1Click:Connect(function()
+    if Toggle.Text == "Close" then
+        Toggle.Text = "Open"
+    else
+        Toggle.Text = "Close"
+    end
     Library:ToggleUI()
 end)
- 
+
 local Corner = Instance.new("UICorner")
 Corner.Name = "Corner"
 Corner.Parent = Toggle
