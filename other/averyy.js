dd377b8f8dc5aef37afbcfe08e47c8fb97d25dc1
BirthX <87570691+BirthScripts@users.noreply.github.com>
2024-04-01 11:22:37 +0800

    averyy

--- other/averyy.js
+++ other/averyy.js
@@ -7,15 +7,13 @@ function playMusic() {
 
 function showContent() {
   const content = [
-    {title: "avery sent this thighs for mod in pistons server lol", imageUrl: "https://cdn.discordapp.com/attachments/1179945316678635601/1212561948831387658/Adpi4pi.jpg?ex=6604be50&is=65f24950&hm=06a76cfbffe9378e6672324f13813a4595a5620dfefa0a134e9ba9e5beed63bd&"},
-    {title: "avery face (why censor mouth)", imageUrl: "https://cdn.discordapp.com/attachments/1179945316678635601/1212561949116596264/888yN79.png?ex=6604be50&is=65f24950&hm=00d96ee71800eb8486d86754174b1abfa69db28730fdcf8c85371de86c452723&"},
-    {title: "tf is this lmao", imageUrl: "https://cdn.discordapp.com/attachments/1179945316678635601/1212561948294778950/nUn5XOo.png?ex=6604be50&is=65f24950&hm=745935c752305eacf2c084031c864eeb29a70fe29b808c3f9ac720e1ec4c341d&"},
-    {title: "avery emo", imageUrl: "https://cdn.discordapp.com/attachments/1179945316678635601/1212562226020483123/WUk4DBh.png?ex=6604be93&is=65f24993&hm=84416228895a628a9651458734d04b0f5f7e62b575f25498e3d07bacdafcabdb&"},
-    {title: "AVERY PEDO", imageUrl: "https://cdn.discordapp.com/attachments/1179945316678635601/1212561951222140968/wNkBDs1.png?ex=6604be51&is=65f24951&hm=e9b19f050befff31d5c5d22adfb93b7b4ddd6c4c1bde25178eae0170e821ea9b&"},
-    {title: "woah", imageUrl: "https://cdn.discordapp.com/attachments/1179945316678635601/1212561950073163776/1I2426B.jpg?ex=6604be51&is=65f24951&hm=9f09d91d4c353c4aaa6903283014f8bbc69777a9fe239ddfed3fd491d7dbb449&"},
-    {title: "wtf bro", imageUrl: "https://cdn.discordapp.com/attachments/1179945316678635601/1212561951670927400/XbwcQJt.png?ex=6604be51&is=65f24951&hm=4567eb0339aacc6721621f7106a23e5341b745dd2fbcbca8fed8d8007ff26362&"},
-    {title: "groomer!!", imageUrl: "https://cdn.discordapp.com/attachments/1179945316678635601/1212694416691822612/e2Di8DB.png?ex=660539af&is=65f2c4af&hm=0157b7296852874a25097f4f8ec7cb5cbbb14761c5eb9f23c1325cfcb298be4a&"},
-    {title: "averyy being wild", imageUrl: "https://cdn.discordapp.com/attachments/1186996277804798003/1217054160084668416/Screenshot_20240227_231729.jpg?ex=6602a103&is=65f02c03&hm=92c73e2401666742b7f237da0e5501029f768541a597c020b6c8911b93695159&"}
+    {title: "avery sent this thighs for mod in pistons server lol", imageUrl: "https://birthscripts.github.io/images/thighs.jpg"},
+    {title: "avery face (why censor mouth)", imageUrl: "https://birthscripts.github.io/images/averyykuromi.png"},
+    {title: "tf is this lmao", imageUrl: "https://birthscripts.github.io/images/tf.png"},
+    {title: "avery emo", imageUrl: "https://birthscripts.github.io/images/edrfger.png"},
+    {title: "AVERY PEDO", imageUrl: "https://birthscripts.github.io/images/edrfger.png/lol.png"},
+    {title: "averyy dealdo", imageUrl: "https://birthscripts.github.io/images/averyyexposed4.png"},
+    {title: "fuck me in the ass", imageUrl: "https://birthscripts.github.io/images/waht.png"},
   ];
 
   const contentContainer = document.querySelector('.content');
